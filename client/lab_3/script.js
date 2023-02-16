2ed57d18c08fa3049e605fb5c314db021277d2c8
rvanculin <rvanculi@umd.edu>
2023-02-16 12:50:36 -0500

    added tags to buttons and updated script.js

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -60,3 +60,9 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
+
+document.querySelector('.prev') // Get the appropriate element (<button class="prev">)
+  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
+    console.log('clicked prev'); // let's tell the client console we made it to this point in the script
+    moveToPrevSlide(); // call the function above to handle this
+  });
