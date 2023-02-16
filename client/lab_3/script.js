258296cc03d6eed8a68b9cf27f23cf830683d727
rvanculin <rvanculi@umd.edu>
2023-02-16 13:26:28 -0500

    updated moveToNextSlide()

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -7,7 +7,7 @@
 */
 /* eslint-enable max-len */
 // set our first slide's position to "0", the opening position in an array
-const slidePosition = 0;
+let slidePosition = 0;
 
 // gather a reference to every slide we're using via the class name and querySelectorAll
 const slides = document.querySelectorAll('.carousel_item');
@@ -26,6 +26,14 @@ function updateSlidePosition() {
 
   // outside your .forEach,
   // add a 'visible' class to the slide at the current slidePosition in slides
+
+  slidesArray.forEach(slide => {
+    slide.classList.remove('visible');
+    slide.classList.add('hidden');
+  });
+
+  slides[slidePosition].classList.add('visible');
+
 }
 
 function moveToNextSlide() {
@@ -35,6 +43,12 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if(slidePosition === totalSlides) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+  
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
